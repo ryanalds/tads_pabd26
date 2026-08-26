8302612aaf4f92700ff25edc17f9822951d6f791
Ryan <ryanalds21@gmail.com>
2026-08-26 11:39:40 -0300

    corrigindo erros na aula 02

--- scripts/aula 02 - revisao DDL.sql
+++ scripts/aula 02 - revisao DDL.sql
@@ -1,86 +1,98 @@
-drop table if exists funcionarios cascade;
-drop table if exists departamentos cascade;
+drop table if exists funcionario cascade;
+drop table if exists departamento cascade;
 
-
-create table funcionarios (
+create table funcionario(
     cpf char(11) primary key,
     pnome varchar(50) not null,
-    unome varchar(50),
+    unome varchar(50) not null,
     email varchar(50) unique,
-    endereco varchar(100),
+    endereco varchar(100) default 'Currais_Novos-RN',
     salario numeric(7,2),
     data_nasc date,
     sexo char(1),
-    cpf_surpervisor char(11),
+    cpf_supervisor char(11),
     numero_departamento smallint,
 
-    constraint funcionario_salrio_check
-    check (salario >= 2000 and salario <= 15000)
+    constraint funcionario_salario_check
+    check (salario >= 2000 and salario <= 15000),
+
+    constraint funcionario_sexo_check
+    check (sexo in ('m', 'f', 'M', 'F', 'o', 'O'))
 );
 
-create table departamentos (
+create table departamento(
     numero smallint primary key,
-    nome varchar(50) not null,
-    cpf_gerente char(11)
+    nome varchar(50) unique,
+    cpf_gerente char(11),
+    data_ini date not null
 );
 
--- Adicionar um novo atributo
+ -- adicioaar restrição FOREIGN KEY:
 
-alter table departamentos
-add column data_ini date;
+alter table funcionario
+add constraint funcionario_num_dep_fk
+foreign key (numero_departamento)
+references departamento(numero)
+-- no action, set null, restrict, cascade, set default
+on delete no action
+on update cascade; 
 
--- Alterar atributo
+SELECT
+    numero_departamento,
+    count(*) AS quantidade_funcionarios
+FROM funcionario
+GROUP BY numero_departamento;
 
-alter table departamentos
-alter column data_ini set not null;
+-- adicionar restrições FK para cpf_supervisor e cpf_gerente
 
--- Excluir atributo
+alter table funcionario
+add constraint funcionario_cpf_sup_fk
+foreign key (cpf_supervisor)
+references funcionario(cpf)
+-- no action, set null, restrict, cascade, set default
+on delete set null
+on update cascade;
 
-alter table departamentos 
+alter table departamento
+add constraint departamento_cpf_gerente_fk
+foreign key (cpf_gerente)
+references funcionario(cpf)
+-- no action, set null, restrict, cascade, set default
+on delete set null
+on update cascade;
+
+-- -- Adicionar um novo atributo
+
+-- alter table departamento
+-- add column data_ini date;
+
+-- -- Alterar um atributo para NOT NULL
+
+-- alter table departamento
+-- alter column data_ini set not null;
+
+-- -- Excluir um atributo
+
+alter table departamento
 drop column data_ini;
 
---Adicionar restrição padrão
+-- -- Adicionar um valor padrão DEFAULT
 
-alter table funcionarios
+alter table funcionario
 alter column endereco set default 'Macau-RN';
 
---Excluir um valor DEFAULT
+-- -- Excluir um valor padrão DEFAULT
 
-alter table funcionarios
-alter column endereco drop default;
+-- alter table funcionario
+-- alter column endereco drop default;
 
--- AAdicionar restrição
+-- -- Adicionar restrição (constraint) CHECK
 
-alter table funcionarios
-add constraint funcionarios_sexo_check
---check (lower(sexo) in ('m', 'f', 'o'));
-check (sexo in ('m', 'f', 'o', 'M', 'F', 'O'));
+-- alter table funcionario
+-- add constraint funcionario_sexo_check
+-- -- check (lower(sexo) in ('m', 'f', 'o'));
+-- check (sexo in ('m', 'f', 'o', 'M', 'F', 'O'));
 
--- Excluir restrição
-
-alter table funcionarios
-drop constraint if exists funcionarios_sexo_check;
-
--- Adicionar restrição FOREING KEY
--- alter table funcionarios
--- add  constraint funcionarios_num_dep_fk
--- foreign key (numero_departamento)
--- references departamentos(numero)
--- -- set null, no action, cascade, set default, restrict
--- on delete no action
--- on update cascade;
-
---
-alter table funcionarios
-add constraint funcionarios_cpf_supervisor_fk
-foreign key (cpf_surpervisor)
-references funcionarios(cpf)
-on delete set null
-on update cascade;
-
-alter table departamentos
-add constraint departamentos_cpf_gerente_fk
-foreign key (cpf_gerente)  
-references funcionarios(cpf)
-on delete set null
-on update cascade;
+-- -- Excluir restrição
+-- alter table funcionario
+-- drop constraint if exists funcionario_sexo_check;
